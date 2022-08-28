04713c9fae0e6e27acc2ce50bf2c1ea15f6cbcc2
Anais Augu <augu.anais@gmail.com>
2022-08-28 13:29:47 +0200

    Informations on contact and news pages
    Informations linked to database so it can be changed after + URL regex validation changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,13 @@ class PagesController < ApplicationController
 	end
 
 	def contact
+    @schedule = Information.where(name: "Horaires").first.content
+    @availability = Information.where(name: "Disponibilités").first.content
 	end
 
   def news
     @posts = Post.all.order(created_at: :desc)
+    @paragraph_1 = Information.where(name: "Paragraphe 1").first.content
+    @paragraph_2 = Information.where(name: "Paragraphe 2").first.content
   end
 end
